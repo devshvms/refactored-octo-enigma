c522ce375bc4b88c8554266580f2203cc5323fd6
shivam <dev.shvms@gmail.com>
2022-09-03 16:15:16 +0530

    Initial commit

---
+++ src/main/java/in/dev/shvms/refactoredoctoenigma/RefactoredOctoEnigmaApplication.java
@@ -0,0 +1,13 @@
+package in.dev.shvms.refactoredoctoenigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoredOctoEnigmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoredOctoEnigmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/dev/shvms/refactoredoctoenigma/RefactoredOctoEnigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dev.shvms.refactoredoctoenigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoredOctoEnigmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
